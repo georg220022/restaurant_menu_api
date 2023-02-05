bd01cbaa49d7686854a89b8c182a32708dd123b9
georg220022 <Bhkkcfgb@yandex.ru>
2023-02-05 12:19:45 +0300

    fix import

--- app/tests_package/run_test.py
+++ app/tests_package/run_test.py
@@ -1,6 +1,8 @@
+"""Импорты тестов стоят в порядке их выполнения! меню -> подменю -> блюда.
+Смена порядка выполнения приведет к ошибкам в тестах!!!"""
 from tests_package.conftest import async_app_client, event_loop  # NOQA
-from tests_package.restaurant_api_test.v1.test_crud_dish import TestGroupDish  # NOQA
 from tests_package.restaurant_api_test.v1.test_crud_menu import TestGroupMenu  # NOQA
 from tests_package.restaurant_api_test.v1.test_crud_sub_menu import (  # NOQA
     TestGroupSubMenu,
 )
+from tests_package.restaurant_api_test.v1.test_crud_dish import TestGroupDish  # NOQA
